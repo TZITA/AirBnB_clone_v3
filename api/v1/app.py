49bd4c7a596e983216f0a082f640fb2bd5d882b9
Thomas Beyene <thomas.beyene.987@gmail.com>
2023-04-29 13:43:23 +0300

    Task 5 Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
 """Flask instance and Blueprint"""
 from os import getenv
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Returns custom 404 error"""
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 @app.teardown_appcontext
