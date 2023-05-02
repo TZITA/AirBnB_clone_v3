2ac66f440380fca0c4a3c9be5d3fe2a9959692b7
Roro1839 <ronaldokipkirui90@gmail.com>
2023-05-02 06:25:12 -0700

    building a restapi

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -1,59 +1,55 @@
 #!/usr/bin/python3
-"""restful API functions for User"""
+
+""" Handles all restful API actions for User"""
+
 from api.v1.views import app_views
+from flask import request, jsonify, abort
 from models.user import User
 from models import storage
-from flask import request, jsonify, abort
 
 
-@app_views.route("/users",
-                 strict_slashes=False,
-                 methods=["GET", "POST"]
-                 )
-@app_views.route("/users/<user_id>",
-                 strict_slashes=False,
-                 methods=["DELETE", "PUT", "GET"])
-def user_end_points(user_id=None):
-    """to get users"""
-    obj_users = storage.all(User)
-    my_dict = [obj.to_dict() for obj in obj_users.values()]
+@app_views.route('/users',
+                 methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/users/<user_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def users(user_id=None):
+    """Retrieves a list of user objects"""
+
+    users_objs = storage.all(User)
+
+    users = [obj.to_dict() for obj in users_objs.values()]
     if not user_id:
-        if request.method == "GET":
-            return jsonify(my_dict), 200
+        if request.method == 'GET':
+            return jsonify(users)
+        if request.method == 'POST':
+            my_dict = request.get_json()
 
-        elif request.method == "POST":
-            imput = request.get_json()
-            if not imput:
-                abort(400, "Not a JSON")
-            if not imput.get("email"):
-                abort(400, "Missing email")
-            if not imput.get("password"):
-                abort(400, "Missing password")
-            else:
-                new_user = User(**imput)
-                new_user.save()
-                return jsonify(new_user.to_dict()), 201
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            if my_dict.get("email") is None:
+                abort(400, 'Missing email')
+            if my_dict.get("password") is None:
+                abort(400, 'Missing password')
+            new_user = User(**my_dict)
+            new_user.save()
+            return jsonify(new_user.to_dict()), 201
     else:
-        if request.method == "GET":
-            new_dict = storage.get(User, user_id)
-            if new_dict:
-                return jsonify(newdict.to_dict()), 200
+        user = storage.get(User, user_id)
+
+        if user is None:
             abort(404)
-        elif request.method == "DELETE":
-            new_dict = storage.get(User, user_id)
-            if new_dict:
-                storage.delete(new_dict)
-                storage.save()
-                return jsonify({}), 200
-            abort(404)
-        elif request.method == "PUT":
-            new_dict = storage.get(User, user_id)
-            get_new_name = request.get_json()
-            if not get_new_name:
-                abort(400, "Not a JSON")
-            for user in obj_users.values():
-                if user.id == user_id:
-                    new_dict.__dict__.update(get_new_name)
-                    new_dict.save()
-                    return jsonify(new_dict.to_dict()), 200
-            abort(404)
+        if request.method == 'GET':
+            return jsonify(user.to_dict())
+        if request.method == 'PUT':
+            my_dict = request.get_json()
+
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            for k, v in my_dict.items():
+                setattr(user, k, v)
+            user.save()
+            return jsonify(user.to_dict()), 200
+        if request.method == 'DELETE':
+            user.delete()
+            storage.save()
+            return jsonify({}), 200
