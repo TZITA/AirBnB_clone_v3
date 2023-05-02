5976404c318b051d8cde076b92d91957c221a2db
Roro1839 <ronaldokipkirui90@gmail.com>
2023-05-02 06:13:16 -0700

    building a restapi

--- api/v1/views/amenities.py
+++ api/v1/views/amenities.py
@@ -1,57 +1,57 @@
 #!/usr/bin/python3
-"""restful API functions for Amenity"""
+
+""" Handles all restful API actions for Amenity"""
+
 from api.v1.views import app_views
+from flask import request, jsonify, abort
 from models.amenity import Amenity
 from models import storage
-from flask import request, jsonify, abort
 
 
-@app_views.route("/amenities",
-                 strict_slashes=False,
-                 methods=["GET", "POST"]
-                 )
-@app_views.route("/amenities/<amenity_id>",
-                 strict_slashes=False,
-                 methods=["DELETE", "PUT", "GET"])
-def amenity_end_points(amenity_id=None):
-    """to get amenities"""
-    obj_amenities = storage.all(Amenity)
-    my_dict = [obj.to_dict() for obj in obj_amenities.values()]
+@app_views.route('/amenities',
+                 methods=['GET', 'POST'], strict_slashes=False)
+@app_views.route('/amenities/<amenity_id>',
+                 methods=['GET', 'PUT', 'DELETE'], strict_slashes=False)
+def amenities(amenity_id=None):
+    """Retrieves a list of amenity objects"""
+    amenity_objs = storage.all(Amenity)
+
+    amenities = [obj.to_dict() for obj in amenity_objs.values()]
     if not amenity_id:
-        if request.method == "GET":
-            return jsonify(my_dict)
+        if request.method == 'GET':
+            return jsonify(amenities)
+        elif request.method == 'POST':
+            my_dict = request.get_json()
 
-        elif request.method == "POST":
-            imput = request.get_json()
-            if not imput:
-                abort(400, "Not a JSON")
-            elif not imput["name"]:
-                abort(400, "Missing name")
-            else:
-                new_amenity = Amenity(**imput)
-                new_amenity.save()
-                return jsonify(new_amenity.to_dict()), 201
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            if my_dict.get("name") is None:
+                abort(400, 'Missing name')
+            new_amenity = Amenity(**my_dict)
+            new_amenity.save()
+            return jsonify(new_amenity.to_dict()), 201
     else:
-        if request.method == "GET":
-            for amenity in my_dict:
+        if request.method == 'GET':
+            for amenity in amenities:
                 if amenity.get('id') == amenity_id:
                     return jsonify(amenity)
             abort(404)
-        elif request.method == "DELETE":
-            for ob in obj_amenities.values():
-                if ob.id == amenity_id:
-                    storage.delete(ob)
+        elif request.method == 'PUT':
+            my_dict = request.get_json()
+
+            if my_dict is None:
+                abort(400, 'Not a JSON')
+            for amenity in amenity_objs.values():
+                if amenity.id == amenity_id:
+                    amenity.name = my_dict.get("name")
+                    amenity.save()
+                    return jsonify(amenity.to_dict()), 200
+            abort(404)
+
+        elif request.method == 'DELETE':
+            for obj in amenity_objs.values():
+                if obj.id == amenity_id:
+                    storage.delete(obj)
                     storage.save()
                     return jsonify({}), 200
-            abort(404)
-        elif request.method == "PUT":
-            new_dict = storage.get("Amenity", amenity_id)
-            get_new_name = request.get_json()
-            if not get_new_name:
-                abort(400, "Not a JSON")
-            for amenity in obj_amenities.values():
-                if amenity.id == amenity_id:
-                    new_dict.name = get_new_name.get("name")
-                    new_dict.save()
-                    return jsonify(new_dict.to_dict()), 200
             abort(404)
